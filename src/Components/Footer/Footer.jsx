b5f30776db69c99f4a146c967a35d011fe67720c
Muhammad Ridha Fatahillah <ridhofatahillah78@gmail.com>
2025-12-14 00:49:24 +0800

    fix: correct image path case sensitivity

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Footer.css";
-import logoFooter from "../../Assets/logo-footer.svg";
-import Phone from "../../Assets/phone.svg";
+import logoFooter from "../../assets/logo-footer.svg";
+import Phone from "../../assets/phone.svg";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
@@ -9,12 +9,18 @@ const Footer = () => {
     <div className="footer" id="footer">
       <div className="footer-content">
         <div className="footer-content-left">
-          <img src={logoFooter} alt="" className="footer-logo" />
+          <img
+            src={logoFooter}
+            alt=""
+            className="footer-logo"
+          />
           <div className="footer-text">
             <h3>Resik Moyudan</h3>
             <p>
-              Jl. Klangon - Tempel No.KM 2, RW.4, Barepan, Sumberrahayu, Kec.
-              Moyudan, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55563
+              Jl. Klangon - Tempel No.KM 2, RW.4,
+              Barepan, Sumberrahayu, Kec. Moyudan,
+              Kabupaten Sleman, Daerah Istimewa
+              Yogyakarta 55563
             </p>
           </div>
         </div>
@@ -23,7 +29,11 @@ const Footer = () => {
             <h3>Hubungi Kami</h3>
           </div>
           <div className="contact">
-            <img src={Phone} alt="" className="phone-icon" />
+            <img
+              src={Phone}
+              alt=""
+              className="phone-icon"
+            />
             <span>082338798647</span>
           </div>
         </div>
@@ -32,14 +42,18 @@ const Footer = () => {
             <h3>Tautan Cepat</h3>
             <ul>
               <Link to="/">Home</Link>
-              <Link to="/SedekahSampah">Sedekah Sampah</Link>
+              <Link to="/SedekahSampah">
+                Sedekah Sampah
+              </Link>
               <Link to="/Edukasi">Edukasi</Link>
               <Link to="/AboutUs">About Us</Link>
             </ul>
           </div>
         </div>
       </div>
-      <p className="footer-copyright">© 2025 Resik Moyudan</p>
+      <p className="footer-copyright">
+        © 2025 Resik Moyudan
+      </p>
     </div>
   );
 };
